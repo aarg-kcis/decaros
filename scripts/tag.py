ad61517acd0721ece610d1334a4104dc8e4cf190
Abhay <abhayrawat2695@gmail.com>
2018-06-03 02:51:07 +0530

    ..

--- scripts/tag.py
+++ scripts/tag.py
@@ -95,9 +95,8 @@ def shutdown():
     rospy.sleep(1)
 
 if __name__ == '__main__':
+    rospy.init_node("tag", anonymous=True)
     MY_ADDRESS  = rospy.get_param("~id")
-    node_name = "tag_{}".format(MY_ADDRESS)
-    rospy.init_node(node_name)
     print("Tag Address: {}".format(MY_ADDRESS))
     init()
     try:
