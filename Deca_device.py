dfc4ae2ebaabd43a348d74d136f7a83c2b30eac1
Abhay <abhayrawat2695@gmail.com>
2018-06-02 21:23:19 +0530

    Initial commit

---
+++ Deca_device.py
@@ -0,0 +1,33 @@
+import DW1000
+import DW1000Constants as C
+
+class DecaDevice:
+    TAG     = 0
+    ANCHOR  = 1
+    def __init__(self, address, type_of_tag):
+        self.address                = address
+        self.type                   = type_of_tag
+        self.is_inactive            = False
+        self.sequenceNumber         = 0
+        self.expectedMessage        = C.POLL_ACK if (self.type == DW1000Device.ANCHOR) else C.POLL
+        
+    def deletePreviousSequenceData(self):
+        for i in self.timestamps:
+            for j in i.keys():
+                if j != self.sequenceNumber-1:
+                    del i[j]
+
+    def is_inactive(self):
+        return is_inactive
+
+    def activate(self):
+        self.is_inactive = False
+
+    def deactivate(self):
+        self.is_inactive = True
+
+    def __str__(self):
+        str = ""
+        d   = self.__dict__
+        for key in d:
+            str += "{}: {}\n".format(key, d[key])
---
+++ test.py
@@ -0,0 +1,12 @@
+def init():
+	global a
+	global b
+	a = 8
+	b =0
+
+def p():
+	print a
+	print b
+
+init()
+p()
